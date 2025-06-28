4944a9dd45fc911e0d07b1b1ffccaeada7d4502d
Jayesh gehlot <jaygehlot99810@gmail.com>
2025-06-29 00:43:23 +0530

    first commit on dev

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 
 app.get('/', async (req, res) => {
-    res.send('Hello CI CD');
+    res.send('Checking CI CD');
 });
 
 app.post('/signup', async (req, res) => {
