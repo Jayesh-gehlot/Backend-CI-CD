6907027fa6ee9edf5655fcefa4ee228e359b76e5
Jayesh gehlot <jaygehlot99810@gmail.com>
2025-06-29 17:35:29 +0530

    checking https

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 
 app.get('/', async (req, res) => {
-    res.send('Checking final production CI CD');
+    res.send('Checking the https production CI CD');
 });
 
 app.post('/signup', async (req, res) => {
